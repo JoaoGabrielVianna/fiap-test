28d129373f937f1acfc52c1ff2496759d4a15b03
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-06-21 19:33:02 -0300

    feat: create a routes and home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Tela(s) | Router(s)
 import App from './routers/app/App'
+import Home from './pages/home/Home'
+
 import './index.scss'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  errorElement: '', //Diferencial
+  children: [
+    {
+      path: '/',
+      element: <Home/>
+    }
+  ]
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- src/routers/app/App.jsx
+++ src/routers/app/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>Welcome to Fiap Test!</div>
-  )
+  return <Outlet />
 }
 
 export default App;
