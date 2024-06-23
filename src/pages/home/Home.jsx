17a3419ea1576847c80f93f1ae3e372d8ddc658e
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-06-23 02:56:57 -0300

    feat: finish (`Home`) the first part of the project,

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
-import './Home.scss'
+import React from 'react';
+
+// Componentes importados
+import Header from '../../components/header/Header';
+import Intro from '../../assets/imgs/intro.png';
+import Marquee from '../../components/marquee/Marquee';
+import WaterScrollImages from '../../components/water-scroll-images/WaterScrollImages';
+
+import './Home.scss';
+import CoursesSection from '../../components/courses-section/CoursesSection ';
+import FaqSection from '../../components/faq-section/FaqSection';
 
 function Home() {
   return (
-    <div>Welcome to Home Page!</div>
-  )
+    <div className='home-container'>
+      <Header />
+
+      {/* Seção 1 - Título: A Melhor Faculdade de Tecnologia */}
+      <section>
+        <div className="section-wrapper">
+          <div className='title'>
+            <h1 className='a-melhor-faculdade typing'>A Melhor Faculdade</h1>
+            <h1 className='typing'>de Tecnologia</h1>
+            <div className="about">
+              <h1>SOB<br />RE</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Seção 2 - Marquee (letreiro) com duas mensagens diferentes */}
+      <section>
+        <Marquee text='CURSOS E IMERSÕES. UMA NOVA CULTURA DE MERCADO.' />
+        <Marquee text='TECNOLOGIA, INOVAÇÃO E NEGÓCIOS. PRESENTE E FUTURO.' direction='ltr' />
+      </section>
+
+      {/* Seção 3 - Imagem de introdução */}
+      <section>
+        <img src={Intro} alt="Imagem de Introdução" />
+      </section>
+
+      {/* Seção 4 - Marquee (letreiro) com duas mensagens diferentes no modo 2 */}
+      <section>
+        <Marquee mode={2} text='SKILLS &bull; CONHECIMENTO &bull; SKILLS' />
+        <Marquee mode={2} text='MUITO. MUITO ALÉM DOS TUTORIAIS' direction='ltr' />
+      </section>
+
+      {/* Seção 5 - Animação de Imagens com Scroll de Água */}
+      <section>
+        <WaterScrollImages className='water' />
+      </section>
+
+      {/* Seção 6 - Cursos */}
+      <section>
+        <CoursesSection />
+      </section>
+      
+    <section>
+      <FaqSection/>
+    </section>
+
+    </div>
+  );
 }
 
 export default Home;
