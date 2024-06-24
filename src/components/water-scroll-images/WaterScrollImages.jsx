96d52ef7d3791bb287aa8aafcf5f069aa5bf2751
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-06-24 11:47:33 -0300

    feat: update a `WaterScrollImages`

--- src/components/water-scroll-images/WaterScrollImages.jsx
+++ src/components/water-scroll-images/WaterScrollImages.jsx
@@ -1,19 +1,16 @@
-// WaterScrollImages.jsx
 import React, { useState, useEffect } from 'react';
 import images from '../../assets/imgs/water';
 
-const WaterScrollImages = ({ className = '' }) => {
-
+const WaterScrollImages = ({ className = '', speed = 1.5 }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [scrollFraction, setScrollFraction] = useState(0);
 
   const handleScroll = () => {
     const scrollTop = window.pageYOffset;
     const maxScrollTop = document.documentElement.scrollHeight - window.innerHeight;
-    const scrollFraction = ((scrollTop - (maxScrollTop / 2)) / (maxScrollTop / 2)) * 2;
-    const newImageIndex = Math.min(
-      images.length - 1,
-      Math.floor(scrollFraction * images.length)
-    );
+    const fraction = (scrollTop / maxScrollTop) * 100;
+    setScrollFraction(fraction);
+    const newImageIndex = Math.min(images.length - 1, Math.floor((fraction / 100) * images.length * speed));
     setCurrentImageIndex(newImageIndex);
   };
 
