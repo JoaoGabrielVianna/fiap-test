7f1665af731f365305d8315f794e8da596825db3
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-06-25 19:59:02 -0300

    feat: Update a `TextTyper`

--- src/components/text-typer/TextTyper.jsx
+++ src/components/text-typer/TextTyper.jsx
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from "react";
 
-const TextTyper = ({ text = "", interval = 100, delay = 0, Markup = "span" }) => {
+const TextTyper = ({ text = "", interval = 100, delay = 0, Markup = "span", className }) => {
   const [typedText, setTypedText] = useState("");
 
-  const typingRender = (text, updater, interval, delay) => {
-    let localTypingIndex = 0;
-    let localTyping = "";
-    
-    setTimeout(() => {
-      let printer = setInterval(() => {
-        if (localTypingIndex < text.length) {
-          updater((localTyping += text[localTypingIndex]));
-          localTypingIndex += 1;
-        } else {
-          localTypingIndex = 0;
-          localTyping = "";
-          clearInterval(printer);
+  useEffect(() => {
+    let typingTimer;
+    let typingIndex = 0;
+    let typing = "";
+
+    const typingAnimation = () => {
+      typingTimer = setTimeout(() => {
+        typing += text[typingIndex];
+        setTypedText(typing);
+        typingIndex++;
+
+        if (typingIndex < text.length) {
+          typingAnimation();
         }
       }, interval);
-    }, delay);
-  };
+    };
 
-  useEffect(() => {
-    typingRender(text, setTypedText, interval, delay);
-  }, [text, interval, delay]);
+    typingAnimation();
+
+    return () => clearTimeout(typingTimer);
+  }, [text, interval]);
 
   return (
     <Markup>
-      <h1 className="typing">{typedText}</h1>
+      <h1 className={`${className} typing`}>{typedText}</h1>
     </Markup>
   );
 };
